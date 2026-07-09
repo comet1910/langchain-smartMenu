4155194b5e36b78b3472143a27330423ac06e3a3
comet1910 <comet1910@163.com>
2026-07-09 22:31:32 +0800

    phase1 查询菜品tool OK

--- agent/langchain_assitant.py
+++ agent/langchain_assitant.py
@@ -1,46 +1,17 @@
 """
 agent助手代码
 """
+import json
+from multiprocessing import Value
+
 from langchain.tools import tool
 import pymysql
 import psycopg2
+from psycopg2.extras import RealDictCursor
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
-@tool
-def search_main_dishes_mysql():
-    """
-    搜索餐厅中的主菜（MySQL版本）
-    """
-
-    with pymysql.connect(host=os.getenv("MYSQL_HOST"),
-                        port=int(os.getenv("MYSQL_PORT", 3306)),
-                        user=os.getenv("MYSQL_USER"),
-                        passwd=os.getenv("MYSQL_PASSWORD"),
-                      ) as conn:
-        with conn.cursor() as cursor:
-            sql = """
-            select
-                dish_name,
-                price,
-                description,
-                category,
-                spice_level,
-                flavor,
-                main_ingredients,
-                cooking_method,
-                is_vegetarian,
-                allergens
-            from 
-                smart_menu.menu_items
-            where
-                is_featured =1 
-
-            """
-            cursor.execute(sql)
-            results = cursor.fetchall()
-            return results
 
 
 @tool
@@ -48,6 +19,18 @@ def search_main_dishes():
     """
     搜索餐厅中的主菜
     """
+    key_name_mapping = {
+        "dish_name":"主菜名称",
+        "price":"价格",
+        "description":"描述",
+        "category":"分类",
+        "spice_level":"辣度等级",
+        "flavor":"口味",
+        "main_ingredients":"主要食材",
+        "cooking_method":"烹饪方法",
+        "is_vegetarian":"是否为素食",
+        "allergens":"过敏信息"
+    }
 
     with psycopg2.connect(host=os.getenv("POSTGRES_HOST"),
                           port=int(os.getenv("POSTGRES_PORT", 5432)),
@@ -55,7 +38,7 @@ def search_main_dishes():
                           password=os.getenv("POSTGRES_PASSWORD"),
                           dbname=os.getenv("POSTGRES_DATABASE", "postgres"),
                           ) as conn:
-        with conn.cursor() as cursor:
+        with conn.cursor(cursor_factory=RealDictCursor) as cursor:
             sql = """
             select
                 dish_name,
@@ -76,7 +59,16 @@ def search_main_dishes():
             """
             cursor.execute(sql)
             results = cursor.fetchall()
-            return results
+           
+            #将数据封装成json
+            json_results = []
+            for item in results:
+                json_item = {}
+                for key,value in item.items():
+                    json_item[key_name_mapping[key]] = value
+                json_results.append(json_item)
+            
+            return json_results
 
 
 
